ea0a68b2476de4ce1fdc8f98fdd5a1a0d8bef52d
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-04 17:40:10 +0800

    server.js: Added routed messages

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 
+import messages from './routes/messages.js'
+
 // App config
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// Middlewares
 app.use( cors() )
 app.use( express.json() )
+
+// Routes
+app.use( `/api/v${process.env.VERSION}/messages`, messages )
 
 app.get( '/', ( req, res ) => res.send('Health check') )
 
