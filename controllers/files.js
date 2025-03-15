4e4b9754b905ccfa8e29fd3041aca59bf31bb0ab
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-16 00:51:19 +0800

    controllers\files.js: Added getFiles and deleteFile controllers

--- controllers/files.js
+++ controllers/files.js
@@ -2,15 +2,13 @@ import asyncWrapper from '../middleware/asyncWrapper.js'
 
 // Firebase
 import firebase from '../firebase.js'
-import { getStorage, ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
+import { getStorage, ref, getDownloadURL, uploadBytesResumable, listAll, deleteObject } from 'firebase/storage'
 
 
 const storage = getStorage(firebase)
 
 const uploadFile = asyncWrapper( async ( req, res ) => {
   try {
-    console.log({req})
-    
     if( !req.file ) {
       return res.status(400).send('No file uploaded.')
     }
@@ -39,6 +37,42 @@ const uploadFile = asyncWrapper( async ( req, res ) => {
   }
 } )
 
+const getFiles = asyncWrapper( async ( req, res ) => {
+  try {
+    // Create a reference under which you want to list
+    const listRef = ref( storage, process.env.UPLOAD_PATH )
+
+    // Find all the prefixes and items.
+    const { items } = await listAll(listRef)
+    
+    const transformedData = await Promise.all( items.map( async ( { name } ) => ( 
+      { name,
+        originalName: name.split('_')[1],
+        downloadURL: await getDownloadURL( ref( storage, `${ process.env.UPLOAD_PATH }/${ name }` ) )
+      } 
+    ) ) )
+
+    res.status(200).json( { files: transformedData } )
+  } catch (error) {
+    console.error(`Failed to get files: ${error}`)
+    res.status(500).json( { message: 'Server error: Failed to get files' } )
+  }
+} )
+
+const deleteFile = asyncWrapper( async ( req, res ) => {
+  try {
+    const { fileName } = req.params
+    
+    await deleteObject( ref( storage, `${ process.env.UPLOAD_PATH }/${ fileName }` ) )
+    res.status(200).json( { message: 'File deleted successfully' } )
+  } catch (error) {
+    console.error(`Failed to delete file: ${error}`)
+    res.status(500).json( { message: 'Server error: Failed to delete file' } )
+  }
+} )
+
 export {
-  uploadFile
+  uploadFile,
+  getFiles,
+  deleteFile
 }
